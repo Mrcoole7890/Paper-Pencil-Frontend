d8f68753ce898d8eb61f2b795ea554ece59cfdc0
Cole A. Kendall <kendallcole7@gmail.com>
2022-09-08 22:18:40 -0400

    add boiler plate code for sending post request to backend to login to the application

--- paper-n-pencil-frontend/src/app/app.component.ts
+++ paper-n-pencil-frontend/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.http.post<any>('http://localhost:8081/login', { userName: 'ColeKendall', password: 'password' }).subscribe(data => {
+            console.log("data sent!");
+    })
+  }
+
   title = 'paper-n-pencil-frontend';
 }

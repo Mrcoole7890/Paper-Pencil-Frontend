9feca75c0fae59960a5ff1c4193c58ac2abfef59
Cole Kendall <kendallcole7@gmail.com>
2022-09-26 19:35:04 -0400

    add error and success messages for login and register components

--- paper-n-pencil-frontend/src/app/login/login.component.ts
+++ paper-n-pencil-frontend/src/app/login/login.component.ts
@@ -10,6 +10,10 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 export class LoginComponent implements OnInit {
 
+  errorFound = false;
+  errorMessage = "";
+  classesOfAlert = "alert alert-warning alert-dismissible fade";
+
   constructor(private http: HttpClient) {
 
   }
@@ -18,8 +22,25 @@ export class LoginComponent implements OnInit {
 
   attemptLogin(username: string, password: string) {
     this.http.post<any>('http://localhost:8081/login', { userName: username, password: password }).subscribe(data => {
-      console.log("data sent!");
+      if (this.isErrorFound(data) == 0) this.userLoginSuccess();
+      else if (this.isErrorFound(data) == 1) this.userLoginIncorrect();
     });
   }
+
+  userLoginSuccess() {
+    this.errorMessage = "Welcome!";
+    this.classesOfAlert = "alert alert-success alert-dismissible fade show";
+  }
+
+  userLoginIncorrect() {
+    this.errorMessage = "This username and/or password are incorrect.";
+    this.classesOfAlert = "alert alert-danger alert-dismissible fade show"; 
+  }
+
+  isErrorFound(responce: any) {
+    if(responce.status == "UNotF") return 1;
+    else if (responce.status == "Good") return 0;
+    else return 0;
+  } 
 
 }
--- paper-n-pencil-frontend/src/app/register/register.component.ts
+++ paper-n-pencil-frontend/src/app/register/register.component.ts
@@ -8,17 +8,36 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor(private http: HttpClient) {
+  errorFound = false;
+  errorMessage = "";
+  classesOfAlert = "alert alert-warning alert-dismissible fade";
 
+  constructor(private http: HttpClient) {
   }
 
   ngOnInit(): void { }
 
   attemptRegister(username: string, password: string) {
     this.http.post<any>('http://localhost:8081/register', { userName: username, password: password }).subscribe(data => {
-      console.log("data sent!");
+      if (this.isErrorFound(data) == 0) this.userRegisterSuccess();
+      else if (this.isErrorFound(data) == 1) this.userRegisterExists();
     });
   }
+
+  userRegisterSuccess() {
+    this.errorMessage = "User has been registered!";
+    this.classesOfAlert = "alert alert-success alert-dismissible fade show";
+  }
+
+  userRegisterExists() {
+    this.errorMessage = "This user exists already.";
+    this.classesOfAlert = "alert alert-warning alert-dismissible fade show"; 
+  }
+
+  isErrorFound(responce: any) {
+    if(responce.status == "USInUse") return 1;
+    else return 0;
+  } 
 
 
 }

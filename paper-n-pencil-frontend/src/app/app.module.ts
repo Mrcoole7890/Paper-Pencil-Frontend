7e73dcb988ea8239bde075505f0b805a4f1c1398
Cole Kendall <kendallcole7@gmail.com>
2022-09-28 23:03:02 -0400

    add formating and a button for navigating to the register component

--- paper-n-pencil-frontend/src/app/app.module.ts
+++ paper-n-pencil-frontend/src/app/app.module.ts
@@ -1,24 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
-import { LoginErrorAlertComponent } from './login-error-alert/login-error-alert.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    RegisterComponent,
-    LoginErrorAlertComponent
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot([
+        {path: 'app-login', component: LoginComponent},
+        {path: 'app-register', component: RegisterComponent},
+        {path: '', redirectTo: '/app-login', pathMatch: 'full'},
+        {path: 'app-register-redirect', redirectTo: '/app-register', pathMatch: 'full'}
+  ]),
   ],
   providers: [],
   bootstrap: [AppComponent]

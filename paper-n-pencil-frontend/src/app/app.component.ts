10bea34be9aa73b6da24ad2a7ec9851ccd1ccb28
Cole Kendall <kendallcole7@gmail.com>
2022-10-06 07:38:15 -0400

    remove prototype code

--- paper-n-pencil-frontend/src/app/app.component.ts
+++ paper-n-pencil-frontend/src/app/app.component.ts
@@ -10,25 +10,9 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 export class AppComponent implements OnInit {
   constructor(private http: HttpClient) { }
 
-  getValidation(token: string): void {
-    this.http.get<any>('http://localhost:8081/user/validateToken', { headers: new HttpHeaders({'auth': token})}).subscribe(res => {
-      console.log(res.message);
-    });
-  }
+  
 
   ngOnInit(): void {
-
-    var token = ""
-
-    this.http.post<any>('http://localhost:8081/user/generateToken', { userName: 'ColeKendall', password: 'password' }).subscribe(data => {
-              token = data.generatedToken;
-              console.log("Token Returned: " + token);
-              this.getValidation(token);
-    })
-
-
-    
-
   }
 
   title = 'paper-n-pencil-frontend';

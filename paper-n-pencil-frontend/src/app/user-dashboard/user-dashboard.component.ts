313e02f28f84baf2ae875f3ddf606b962889ca4e
Cole Kendall <cole.kendall.18@cnu.edu>
2022-12-01 03:07:30 -0500

    add api calls to backend for memebers and base tables

--- paper-n-pencil-frontend/src/app/user-dashboard/user-dashboard.component.ts
+++ paper-n-pencil-frontend/src/app/user-dashboard/user-dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-user-dashboard',
@@ -7,19 +9,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserDashboardComponent implements OnInit {
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
   token = sessionStorage.getItem('token');
-
   ngOnInit(): void {
+    console.log(this.token)
+    this.getClanData();
+    this.getUserData();
+    this.getBaseData();
+    this.getMembersData();
   }
 
   getBaseData() {
-
+    const headers = new HttpHeaders().set('auth', this.token + "");
+    const body = { };
+    this.http.post<any>('http://localhost:8081/base', body, {headers}).subscribe(data => {
+      console.log(data);
+    });
   }
 
   getClanData() {
-    
+    const headers = new HttpHeaders().set('auth', this.token + "");
+    const body = { };
+    this.http.post<any>('http://localhost:8081/clan', body, {headers}).subscribe(data => {
+      console.log(data);
+    });
   }
 
+  getUserData() {
+    const headers = new HttpHeaders().set('auth', this.token + "");
+    const body = { };
+    this.http.post<any>('http://localhost:8081/user', body, {headers}).subscribe(data => {
+      console.log(data);
+    });
+  }
+
+  getMembersData() {
+    const headers = new HttpHeaders().set('auth', this.token + "");
+    const body = { };
+    this.http.post<any>('http://localhost:8081/memebers', body, {headers}).subscribe(data => {
+      console.log(data);
+    });
+  }
 }
